f1589b7bcfc548cedb83ddba8e629bcbb0946bfd
Bohdan Matsko <bm01271970@gmail.com>
2024-07-05 14:32:59 +0200

    +not ended work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,19 @@
-import { Suspense } from "react";
-import "./App.css";
+import { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { refreshUser } from "../../redux/auth/operations";
+
+import "./App.css";
+
 import HomePage from "../../pages/HomePage";
 import RegistrationPage from '../../pages/RegistrationPage'
-import AppBar from "../AppBar/AppBar";
 import LoginPage from "../../pages/LoginPage";
 import ContactsPage from "../../pages/ContactsPage";
 import PrivateRoute from '../PrivateRoute/PrivateRoute'
 import RestrictedRoute from "../RestrictedRoute/RestrictedRoute";
+import Layout from "../Layout/Layout";
 
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import {refreshUser} from '../../redux/auth/operations'
 
 function App() {
 
@@ -22,21 +24,23 @@ function App() {
 
   return (
     <>
-      <AppBar />
       <Suspense fallback={null}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-            
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+
             <Route element={<RestrictedRoute />}>
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegistrationPage />} />
             </Route>
-          
+
             <Route element={<PrivateRoute />}>
               <Route path="/contacts" element={<ContactsPage />} />
             </Route>
 
-          <Route path="*" element={<div>404</div>} />
+            <Route path="*" element={<div>404</div>} />
+          </Route>
+
         </Routes>
       </Suspense>
     </>
